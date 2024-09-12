cd9498611f93732537fd63a2ab6fba37676af9cc
asnts <annandasants@gmail.com>
2024-09-12 11:54:06 -0300

    add constructor into class RecintosZoo

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,4 +1,16 @@
 class RecintosZoo {
+    constructor(){
+       this.Recintos = [
+        {
+            numero: 1, bioma: 'savana', tamanho: 10, animais: [{especie: 'macaco', quantidade: 3, tamanho:1}],
+            numero: 2, bioma: 'floresta', tamanho: 5, animais: [], 
+            numero: 2, bioma: 'savana e rio', tamanho: 7, animais: [{especie: 'gazela', quantidade: 1, tamanho:2}], 
+            numero: 2, bioma: 'rio', tamanho: 10, animais: [], 
+            numero: 2, bioma: 'savana', tamanho: 10, animais: [], 
+            
+        }
+       ]
+    }
 
     analisaRecintos(animal, quantidade) {
     }
